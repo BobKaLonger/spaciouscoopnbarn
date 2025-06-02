3bceef3de07686529fdebcbd236121a8a7037e75
BobKaLonger <26144307+BobKaLonger@users.noreply.github.com>
2025-06-02 15:56:56 -0400

    idk if this is even right LOL LOL

--- Code/CompatibilityModulator.cs
+++ Code/CompatibilityModulator.cs
@@ -25,7 +25,38 @@ namespace spaciouscoopnbarn
         public bool checkCompatibilities(IModHelper Helper)
         {
             helper = Helper;
+            var compatibilities = helper.Data.ReadJsonFile<Dictionary<string, Compatibility>>(PathUtilities.NormalizePath("assets/Compatibilies.json"));
+            if (compatibilities is null)
+            {
+                Log.Error("Compatibility list file not found!");
+                return false;
+            }
+            foreach (var compatibility in compatibilities)
+            {
+                if (!helper.ModRegistry.IsLoaded("FlashShifter.StardewValleyExpandedCP"))
+                {
+                    Log.Trace($"Stardew Valley Expanded is not installed, skipping compatibility!");
+                    hasSVE = false;
+                }
+
+                if (!helper.ModRegistry.IsLoaded("bobkalonger.gigacoopnbarn"))
+                {
+                    Log.Trace($"Gigantic Coop and Barn is not installed, skipping compatibility!");
+                    hasBKGCB = false;
+                }
             
+                if (!helper.ModRegistry.IsLoaded("jenf1.megacoopbarn"))
+                {
+                    Log.Trace($"Jen's Mega Coop and Barn is not installed, skipping compatibility!");
+                    hasJMCB = false;
+                }
+
+                if (!helper.ModRegistry.IsLoaded("UncleArya.ResourceChickens"))
+                {
+                    Log.Trace($"Resource Chickens is not installed, skipping compatibility!");
+                    hasUARC = false;
+                }
+            }
         }
     }
 }
