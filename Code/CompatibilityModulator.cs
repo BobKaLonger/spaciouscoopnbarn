7f0b2f52b8d94cb4636eb3f1760d0b9734216fb0
BobKaLonger <26144307+BobKaLonger@users.noreply.github.com>
2025-06-02 23:47:52 -0400

    obinkekjf

--- Code/CompatibilityModulator.cs
+++ Code/CompatibilityModulator.cs
@@ -22,41 +22,25 @@ namespace spaciouscoopnbarn
         bool hasJMCB = true;
         bool hasUARC = true;
 
-        public bool checkCompatibilities(IModHelper Helper)
+        public bool checkSVE(IModHelper Helper)
         {
             helper = Helper;
-            var compatibilities = helper.Data.ReadJsonFile<Dictionary<string, Compatibility>>(PathUtilities.NormalizePath("assets/Compatibilies.json"));
-            if (compatibilities is null)
-            {
-                Log.Error("Compatibility list file not found!");
-                return false;
-            }
-            foreach (var compatibility in compatibilities)
-            {
-                if (!helper.ModRegistry.IsLoaded("FlashShifter.StardewValleyExpandedCP"))
-                {
-                    Log.Trace($"Stardew Valley Expanded is not installed, skipping compatibility!");
-                    hasSVE = false;
-                }
+            var compatibilities = ;
 
-                if (!helper.ModRegistry.IsLoaded("bobkalonger.gigacoopnbarn"))
-                {
-                    Log.Trace($"Gigantic Coop and Barn is not installed, skipping compatibility!");
-                    hasBKGCB = false;
-                }
-            
-                if (!helper.ModRegistry.IsLoaded("jenf1.megacoopbarn"))
-                {
-                    Log.Trace($"Jen's Mega Coop and Barn is not installed, skipping compatibility!");
-                    hasJMCB = false;
-                }
+            if (!helper.ModRegistry.IsLoaded("FlashShifter.StardewValleyExpandedCP"))
+                hasSVE = false;
 
-                if (!helper.ModRegistry.IsLoaded("UncleArya.ResourceChickens"))
-                {
-                    Log.Trace($"Resource Chickens is not installed, skipping compatibility!");
-                    hasUARC = false;
-                }
-            }
+            if (!helper.ModRegistry.IsLoaded("bobkalonger.gigacoopnbarn"))
+                hasBKGCB = false;
+            else return hasBKGCB;
+
+            if (!helper.ModRegistry.IsLoaded("jenf1.megacoopbarn"))
+                hasJMCB = false;
+            else return hasJMCB;
+
+            if (!helper.ModRegistry.IsLoaded("UncleArya.ResourceChickens"))
+                hasUARC = false;
+            else return hasUARC;
         }
     }
 }
